e03dc6082229696d000341f60a5361f97f8dbc37
Issa Nyale <me@email.com>
2023-02-13 10:00:55 +0300

    completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,24 @@
 require 'pry'
-
 class String
-
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
-
+  
   def count_sentences
+    split = self.split(/[.!?]+/)
+    split.reject! { |word| word.strip.empty? }
+    split.count
+
+    #binding.pry
 
   end
 end
